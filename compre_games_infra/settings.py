5b00a4c52f54ade3a2d06342f3fe91bcf35b2ebe
Luís Henrique <henrique2345@ufg.br>
2023-02-13 01:34:03 -0300

    Conexão com o Postgres

--- compre_games_infra/settings.py
+++ compre_games_infra/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'compre_games_infra.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
